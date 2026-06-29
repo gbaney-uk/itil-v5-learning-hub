1239a3009bd4897c8f624cab6e65d42a850b0f51
gbaney-uk <gbaney@googlemail.com>
2026-06-29 13:59:32 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,17 @@
-const CACHE_NAME = "itil-v5-learning-hub-v1";
+const CACHE_NAME = "itil-v5-four-dimensions-v1";
 
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./styles.css",
   "./script.js",
-  "./manifest.json"
+  "./manifest.json",
+  "./assets/mindmaps/Products-and-Service.png",
+  "./assets/mindmaps/Organisations-and-people.png",
+  "./assets/mindmaps/Information-and-Technology.png",
+  "./assets/mindmaps/Partners-and-Suppliers.png",
+  "./assets/mindmaps/value-streams-and-processes.png",
+  "./assets/mindmaps/External-Factors.png"
 ];
 
 self.addEventListener("install", (event) => {
